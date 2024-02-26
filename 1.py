c392a3eb44fca26bca9c2013ecff64758554cdf5
TrukhinaPolin <trpa05@gmail.com>
2024-02-26 20:09:11 +0300

    nn

--- 1.py
+++ 1.py
@@ -1,2 +1,8 @@
-a = 2
-print(a)
+a = input("введите пароль ")
+b = input("введите подтверждение пароля ")
+if len(a) > 7 and len(b) > 7:
+    if a == ()
+    if a == b:
+        print("+")
+    else:
+        print("-")
---
+++ 2.py
@@ -0,0 +1,6 @@
+b = int(input("введите место "))
+for a in range(1, 55):
+    if b % 2 == 0:
+        print("верхнее")
+    else:
+        print("нижнее")
