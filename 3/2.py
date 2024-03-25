37801d72148b108dbe94eac83b616ee1dcd64f8e
TrukhinaPolina <trpa05@gmail.com>
2024-03-25 19:57:46 +0300

    1

--- 3/2.py
+++ 3/2.py
@@ -1,8 +1,10 @@
-a = int(input(""))
+
 try:
-    b = 100/a
+    a = int(input("Введите число "))
+    b = 100 / a
+    print(f"b = {b}")
 except ZeroDivisionError:
     print('Ошибка! Деление на 0')
+
 except ValueError:
     print('Ошибка! Деление на строку')
-print(b)
