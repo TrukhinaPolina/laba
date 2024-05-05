a99bdb393eaefe7eb174a667ea9251c7d0cc2a2e
TrukhinaPolina <159804797+TrukhinaPolina@users.noreply.github.com>
2024-05-06 01:06:19 +0300

    2.py

--- 2.py
+++ 2.py
@@ -1,6 +1,9 @@
-b = int(input("введите место "))
-for a in range(1, 55):
-    if b % 2 == 0:
-        print("верхнее")
-    else:
-        print("нижнее")
+b = int(input(" введите место "))
+if b % 2 == 0:
+    print("верхнее")
+else:
+    print("нижнее")
+if 37 <= b <= 54:
+    print("боковое")
+else:
+    print("купе")
